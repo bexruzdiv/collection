cf3c3d18083e1c06de618fa7544a2cb7244c3408
Bexruz Turobjonov <bexruzturobjonov0955@gmail.com>
2024-02-23 20:02:24 +0500

    change backuo

---
+++ vault_collection/roles/vault_backup/templates/installer_rclone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp rclone.1 /usr/local/share/man/man1/
+sudo mandb
+echo "q" | rclone config
